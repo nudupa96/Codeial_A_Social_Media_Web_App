75eebfe5c7db35cf965541636329f1730dec33dc
Nagendra K Udupa <nagudupa@Nagendras-MacBook-Pro.local>
2022-05-21 19:01:26 +0530

    Setup the static files in the Assets folder and link them with main index.js, Modify main index.js to extract styles and scripts of subpages and link the static files with the EJS files accordingly

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 8000;
 //import the express-ejs-layouts lib
 const expressLayouts = require('express-ejs-layouts');
+//link the static files folder called assets
+app.use(express.static('./assets'));
 
 //need to use ejs layouts lib before routes  is required
 app.use(expressLayouts);
+
+//extract styles and scripts from sub pages into thr layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //now we need to tell the app to use the routes folder,ie,
 //all the get,post etc will be handled by this module - This is a MW
