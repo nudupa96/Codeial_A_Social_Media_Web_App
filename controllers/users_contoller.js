d4a3191d3d0f741f0be12006c0ae45df1c5da83e
Nagendra K Udupa <nagudupa@Nagendras-MacBook-Pro.local>
2022-05-21 13:25:05 +0530

    Create Users and Posts EJS files and Render it using Users and Posts Controller

--- controllers/users_contoller.js
+++ controllers/users_contoller.js
@@ -1,3 +1,4 @@
+/* First Time 
 //here I need to export an action
 module.exports.profile = function(req, res){
     res.end('<h1>User Profile Page</h1>');
@@ -5,4 +6,22 @@ module.exports.profile = function(req, res){
 
 module.exports.post = function(req, res){
     res.end('<h1>Posts of the Great User!!</h1><h2>Posttt1</h2><h2>Posttt2</h2>');
+};
+*/
+/* Second Time */
+
+module.exports.profile = function(req, res){
+    res.render('users', {
+        title : "Users Profile", 
+        postname1: "User Git",
+        postname2: "User Github"
+    });
+};
+
+module.exports.post = function(req, res){
+    res.render('users', {
+        title : "Users Post Profile", 
+        postname1: "User Post Git",
+        postname2: "User Post Github"
+    });
 };
